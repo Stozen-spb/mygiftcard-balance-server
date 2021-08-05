c00cc4a395b89fd0a336057c9645d1d7bd0441d6
stozen-spb <2012stalker@gmail.com>
2021-08-05 12:51:25 +0300

    created

---
+++ index.js
@@ -0,0 +1,37 @@
+const http = require('http');
+const cors = require('cors');
+const express = require('express');
+const bodyParser = require('body-parser');
+const axios = require('axios');
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+app.options('*', cors());
+
+
+app.get('/', (req, res) => {
+  res.send('Hello from node.js!!!')
+})
+
+app.post('/getcards', (req,res) => {
+	console.log(req.body.url)
+	const targetURL = req.body.url
+
+	axios.get(targetURL)
+		.then(response => {
+			res.send(response.data)
+		})
+		.catch(error => {
+			const errorText = {
+				errorText: 'Целевой сервер не отвечает',
+				error: error
+			}
+			res.send(errorText)
+		})
+
+	
+})
+
+app.listen(8000, () => {
+        console.log('Server has been started!');
+})
